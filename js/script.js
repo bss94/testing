1985692ddc85a9cddc1a27a4fc7331df234c7e2d
Your Name <you@example.com>
2019-10-10 09:52:51 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $(".teams").owlCarousel({
+        loop: true,
+        responsive: {
+            320: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            900:{
+                items: 4
+            }
+
+        }
+
+    });
+
+
+$(".testimm").owlCarousel({
+    loop: true,
+    items:1,
+
+
+});
+
+
+});
